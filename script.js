ada2c1c9a9636bd133f814f258f1ae647ed884e1
methmiranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2021-11-26 12:52:35 +0530

    3rd commit

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const icons =document.querySelectorAll('.section-1-icons i')
 
+let i =1
 setInterval(()=>{
 
-    const icon=document.querySelector('.section-1-icons.change')
+    const icon=document.querySelector('.section-1-icons i.change')
     icon.classList.remove('change')
     icon.nextElementSibling.classList.add('change')
 
