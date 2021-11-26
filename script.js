10269a85f3c85153a7270408b424140b3bfac574
methmiranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2021-11-26 12:57:48 +0530

    4 th commit

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ const icons =document.querySelectorAll('.section-1-icons i')
 
 let i =1
 setInterval(()=>{
-
+    i++
     const icon=document.querySelector('.section-1-icons i.change')
     icon.classList.remove('change')
-    icon.nextElementSibling.classList.add('change')
+    if(i>icons.length){
+        icons[0].classList.add('change')
+        i=1
+    }
+    else{
+        icon.nextElementSibling.classList.add('change')
+    }
+   
 
-},1000)
+},2000)
